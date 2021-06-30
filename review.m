49eeb93aa1eff8d022d1e07fef54c0a3d928dd58
jennifertramsu <jennifer.tramsu@mail.mcgill.ca>
2021-06-29 23:52:29 -0400

    Creating cheat sheet

---
+++ review.m
@@ -0,0 +1,26 @@
+%%
+%%%%% CHAPTER 1 - Starting with Matlab
+
+%% Command Window
+% Comment with %
+% Suppress outputs with ;
+% Clear command window with clc
+
+%% Display Formats
+% Default is short (4 dp)
+% Change with format command
+% --> format long (15 dp)
+% --> format short e (e indicates scientific notation)
+% --> format bank (2 dp)
+% --> format compact (no empty lines)
+% --> format loose (adds empty lines)
+
+%% Commands to Manage Variables
+% clear - removes all variables from memory
+% clear x y z - removes x y z specifically
+% who - displays list of variables currently in memory
+% whos - displays list of variables and their metadata
+
+%%
+%%%%% CHAPTER 2 - Creating Arrays
+
