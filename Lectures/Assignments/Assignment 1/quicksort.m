15397148c2e697d3b4d3dfc5ed2047e07c6e31c7
jennifertramsu <jennifer.tramsu@mail.mcgill.ca>
2021-09-13 23:08:28 -0400

    starting asgn

---
+++ Lectures/Assignments/Assignment 1/quicksort.m
@@ -0,0 +1,17 @@
+% Assignment 1
+% Jennifer Tram Su 260923530
+
+function quicksort(v, l, r)
+% v = 3D matrix
+% This function will implement the quicksort algorithm on a 3D matrix of
+% any dimension in 2 steps
+% Step 1 = Sort the matric by rearranging the 2 planes such that the plane
+% with the lowest sum of the diagonal elements is at the front
+
+% QUESTIONS
+% Diagonal elements --> only the first diagonal?
+% What if plane is not square
+
+% Step 2 = sort each plane
+
+end
