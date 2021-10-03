8b95298ee6e7ae834336b08e93e619efaf8d138d
jennifertramsu <jennifer.tramsu@mail.mcgill.ca>
2021-10-03 18:39:54 -0400

    i give up

--- Assignments/Assignment 2/main.m
+++ Assignments/Assignment 2/main.m
@@ -7,12 +7,14 @@ Fom = [0, 2];
 r = brent(@(x)x*cot(x^1.5), 0, 20, 1e-12);
 size(r)
 
-%brent(@(x)x, -1, 1, 1e-12)
-%brent(@(x)x^2-1, -1, 1, 1e-12)
-%brent(@(x)x^3, -1, 1, 1e-12)
+r = brent(@(x)sin(x^2), 0, 300, 1e-12);
+size(r)
+
+brent(@(x)x, -1, 1, 1e-12)
+brent(@(x)x^2-1, -1, 1, 1e-12)
+brent(@(x)x^3, -1, 1, 1e-12)
 brent(@(x)sin(x), -pi/2, pi/2, 1e-12)
 brent(@(x)tan(x), -pi/2, pi/2, 1e-12)
-
 
 % brent on hygroscopicity
 
